04d30813069e6ce6435d72a028f7fe00599c0be2
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:59:25 +0800

    New translations lego.js (Chinese Simplified)

--- locales/zh/lego.js
+++ locales/zh/lego.js
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
   KUBE_ASCENDING_ORDER: '正序排列…',
   KUBE_DESCENDING_ORDER: '倒序排列…',
@@ -67,5 +66,5 @@ module.exports = {
   KUBE_RANGE_SEPARATOR: ' 至 ',
   KUBE_SCROLL_TO_INCREMENT: '点击切换 12/24 小时时制',
   KUBE_CLICK_TO_TOGGLE: '滚动切换',
-  KUBE_YEAR: '年',
-}
+  KUBE_YEAR: ''
+};
